bca4d5e6b6ba5a5547cfd091835264949c1446f6
aashumallik <amallik@>
2021-09-06 16:16:46 -0500

    writing first smart contract in solidity

---
+++ MyContract.sol
@@ -0,0 +1,21 @@
+//pragma solidity 0.5.1;
+
+
+// this is essentially making a class
+contract Mycontract {
+
+    string value;
+
+    constructor() public{
+    value="myValue";
+}
+
+function get() public view return(string){
+return value;
+}
+      
+function set(string _value) public{
+value = _value;
+}
+
+}
